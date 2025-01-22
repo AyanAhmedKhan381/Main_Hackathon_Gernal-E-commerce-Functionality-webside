70c1ace50f466d80e6ef5aef45d7b2ee2b2703cb
AyanAhmedKhan381 <Sakratsuperstar@gmail.com>
2025-01-22 23:39:47 +0500

    Gernal E-commerce

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,13 @@
-import { createClient } from 'next-sanity';
+import { createClient } from 'next-sanity'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const apiToken = process.env.SANITY_API_TOKEN
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,  // Valid project ID from your environment
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,       // Valid dataset name from your environment
-  apiVersion: '2023-01-01',  // Example of a valid API version
-  token: process.env.SANITY_API_TOKEN, // Correct token field
-  useCdn: process.env.NODE_ENV === 'production' ? false : true, // Use CDN in development, not in production
-});
+  projectId,
+  dataset,
+  apiVersion: '2023-01-01',
+  token: apiToken,
+  useCdn: process.env.NODE_ENV === 'production',
+})
--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,9 +1,9 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
-import { dataset, projectId } from '../env'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || ''
 
-// https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
